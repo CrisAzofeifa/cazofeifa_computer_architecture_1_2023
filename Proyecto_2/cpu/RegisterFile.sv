41bd984f30d72b9cd14b04a0209b0784c461df90
Daniela Brenes <danibreo6@gmail.com>
2023-05-10 11:40:27 -0600

    Register file terminado

--- Proyecto_2/cpu/RegisterFile.sv
+++ Proyecto_2/cpu/RegisterFile.sv
@@ -1,20 +1,24 @@
 module RegisterFile (
-  input wire clk,
-  input wire [2:0] readAddr1, readAddr2,
-  input wire [2:0] writeAddr,
-  input wire writeEnable,
-  input wire [31:0] writeData,
-  output reg [31:0] readData1, readData2
+    input logic         clk, writeEnable
+    input logic [2:0]   readAddr1, readAddr2, writeAddr,
+    input logic [31:0]  writeData, r9,
+    output logic [31:0] readData1, readData2
 );
 
-  reg [31:0] registers [8:0];
+  logic [31:0] registers [8:0];
+
+  // three ported register file
+  // read two ports combinationally
+  // write third port on rising edge of clock
+  // register 9 reads PC + 8 instead
 
   always_ff @(posedge clk) begin
     if (writeEnable) begin
       registers[writeAddr] <= writeData;
     end
   end
-  assign readData1 = registers[readAddr1];
-  assign readData2 = registers[readAddr2];
+
+  assign readData1 = (readAddr1 == 4'b1001) ? r9 : registers[readAddr1];
+  assign readData2 = (readAddr2 == 4'b1001) ? r9 : registers[readAddr2];
 
 endmodule
