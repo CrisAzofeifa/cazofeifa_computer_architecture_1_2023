f7b93e61cc257dafedcaa8c7bd1d281942598fb6
Daniela Brenes <danibreo6@gmail.com>
2023-05-11 19:40:26 -0600

    Compiler with DP instructions

--- Proyecto_2/script/decoder.py
+++ Proyecto_2/script/decoder.py
@@ -2,8 +2,19 @@
 #TODO CONFIGURAR LAS OPERACIONES
 #TODO VER COMO SE HACE CON LOS LABELS
 
-registers = {
+dp = {
+    'cond+op': '000',
+    'register': '0',
+    'immediate': '1',
+    'SUM': '0000',
+    'MUL': '0010',
+    'DIV': '0100',
+    'MOD': '0110',
+    'MOV': '1000',
+    'EQV': '1011'
+}
 
+registers = {
     'R0': '0000',
     'R1': '0001',
     'R2': '0010',
@@ -15,7 +26,13 @@ registers = {
     'R8': '1000',
     'R9': '1001'
 }
-instruccion = ''
+
+Rd = ''
+Rn = ''
+Src2 = ''
+binary_instr = ''
+list_binary = []
+
 
 def compiler():
     file = open("./code.txt", 'r')
@@ -26,43 +43,64 @@ def compiler():
 
     for instr in instrList:
         lista = instr.split(' ')
-        if lista[0] == 'SUM': #Suma
-            parse(lista)
-        elif lista[0] == 'MUL': #Multiplicacion
-            parse(lista)
-        elif lista[0] == 'DIV': #Division
-            parse(lista)
-        elif lista[0] == 'MOD':  #Modulo
-            parse(lista)
-        elif lista[0] == 'MOV':  #Mov asignacion
-            parse(lista)
-        elif lista[0] == 'EQV':  #Compare
-            print('equal')
-        elif lista[0] == 'GET':  #Load
-            print('guardar')  
-        elif lista[0] == 'STR':  #Store
-            print('pedir') 
-        elif lista[0] == 'SEQ':  #Branch equal
-            print('b equal') 
-        elif lista[0] == 'S': #Branch
-            print('b') 
-        else:               #Label
-            print('label')
 
-def parse(lista):
-    if lista[1] in registers: #Verifica que el registro Rd este en el diccionario
-        registro = registers[lista[1]]
-        print(registro)
+        if lista[0] in dp: 
+            # data-processing instruction
+            binary_instr = dp['cond+op']
+            if len(lista) == 4: 
+                # SUM, MUL, DIV, MOD
+                if lista[3] in registers:
+                    # register-register 
+                    binary_instr += dp['register']
+                    
+                    Src2 =  '000000'+ registers[lista[3]]
+                else:
+                    # register-immediate
+                    binary_instr += dp['immediate']
 
-    if lista[2] in registers: #Verifica que el registro Rn este en el diccionario
-        registro = registers[lista[2]]
-        print(registro)
+                    # se convierte el numero a binario de 10 bits
+                    Src2 = str(bin(int(lista[3]))[2:].zfill(10))
 
-    if lista[3] in registers: #Verifica que el registro Rm este en el diccionario
-        registro = registers[lista[3]]
-        print(registro)
-    else: #Si no es un registro, es un immediate
-        numero = str(bin(int(lista[3]))[2:].zfill(10)) #se convierte el numero a binario de 10 bits
-        print(numero)
+                binary_instr += dp[lista[0]]
+                Rd = registers[lista[1]]
+                Rn = registers[lista[2]]
+
+                binary_instr += Rd
+                binary_instr += Rn
+                binary_instr += Src2
+
+            elif len(lista) == 3:
+                # MOV, EQV
+                if lista[2] in registers:
+                    # register-register 
+                    binary_instr += dp['register']
+                    Rn = registers[lista[2]]
+                    Src2 = '0000000000'
+                    
+                else:
+                    # register-immediate
+                    binary_instr += dp['immediate']
+                    Rn = '0000'
+                    Src2 = str(bin(int(lista[2]))[2:].zfill(10))
+
+                binary_instr += dp[lista[0]]
+                Rd = registers[lista[1]]
+
+                binary_instr += Rd
+                binary_instr += Rn
+                binary_instr += Src2
+
+        # complete 32 bits
+        binary_instr =  '000000' + binary_instr 
+        decimal = int(binary_instr, 2)
+        hexa = hex(decimal)
+        
+        list_binary.append(hexa[2:])
+    
+    f = open ('machine_code.txt','w')
+    for instr in list_binary:
+        f.write(instr + '\n')
+    f.close()
+    print(list_binary)
 
 compiler()
