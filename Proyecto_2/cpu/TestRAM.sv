2a3b3e9a2737f8d38c76e7d7c72a7571a3cc490b
Oscar Méndez <48070861+Oscarmendez825@users.noreply.github.com>
2023-05-15 10:56:50 -0600

    Arreglar test memoria

--- Proyecto_2/cpu/TestRAM.sv
+++ Proyecto_2/cpu/TestRAM.sv
@@ -6,35 +6,31 @@ module TestRAM;
 	logic wren;
 	logic [31:0]q;
 
-	dmem datmem(address, clk, data, wren, q);
+	dmem datmem(clk, wren, address, data, q);
 	 
 	  
 	always begin
 		clk = 0;
 		clk <= 1; #5; clk <= 0; #5;
-	end
-	
-	always @(posedge clk) begin
-	
+		
 		address = 0;
 		wren = 0;
 		data = 0;
-		$display("Data en 0 %d", q);
+		$display("Data en 0 %h", q);
 		#15;
 		
 		address = 10;
 		wren = 0;
 		data = 0;
-		$display("Data en 10 %d", q);
+		$display("Data en 10 %h", q);
 		#15;
 		
 		
 		address = 3;
 		wren = 0;
 		data = 0;
-		$display("Data en 3 %d", q);
+		$display("Data en 3 %h", q);
 		#15;
-	
 	end
 	
 	
--- Proyecto_2/cpu/dmem.sv
+++ Proyecto_2/cpu/dmem.sv
@@ -3,7 +3,7 @@ module dmem (
     input logic [31:0] a, wd,
     output logic [31:0] rd);
 
-    logic [31:0] RAM[90025:0];
+    logic [31:0] RAM[90024:0];
 
     assign rd = RAM[a[31:0]]; // word aligned
 	 
