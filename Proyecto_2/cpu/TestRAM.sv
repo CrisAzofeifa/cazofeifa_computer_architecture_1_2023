94044e06fdce29cdb79ad9d79c0f90cc72e17191
Oscar Méndez <48070861+Oscarmendez825@users.noreply.github.com>
2023-05-15 15:03:07 -0600

    Actualizacion test de memoria

--- Proyecto_2/cpu/TestRAM.sv
+++ Proyecto_2/cpu/TestRAM.sv
@@ -1,12 +1,12 @@
 module TestRAM;
 
-	logic [31:0] address;
+	logic [31:0] address, address2;
 	logic clk;
 	logic [31:0] data;
 	logic wren;
-	logic [31:0]q;
+	logic [31:0] q1, q2;
 
-	dmem datmem(clk, wren, address, data, q);
+	dmem datmem(clk, wren, address, address2, data, q1, q2);
 	 
 	  
 	always begin
@@ -14,23 +14,23 @@ module TestRAM;
 		clk <= 1; #5; clk <= 0; #5;
 		
 		address = 0;
-		wren = 0;
-		data = 0;
-		$display("Data en 0 %h", q);
+		$display("Data en 0 %h", q1);
+		
+		address2 = 25;
+		$display("Data en 25 %h", q2);
+		
 		#15;
 		
-		address = 10;
-		wren = 0;
-		data = 0;
-		$display("Data en 10 %h", q);
+		address = 5;
+		$display("Data en 5 %h", q1);
+		
+		address2 = 9005;
+		$display("Data en 9005 %h", q2);
+		
 		#15;
 		
 		
-		address = 3;
-		wren = 0;
-		data = 0;
-		$display("Data en 3 %h", q);
-		#15;
+		
 	end
 	
 	
--- Proyecto_2/cpu/dmem.sv
+++ Proyecto_2/cpu/dmem.sv
@@ -1,15 +1,16 @@
 module dmem ( 
     input logic clk, we,
-    input logic [31:0] a, wd,
-    output logic [31:0] rd);
+    input logic [31:0] a, a1, wd,
+    output logic [31:0] rd, rd2);
 
     logic [31:0] RAM[90024:0];
 
     assign rd = RAM[a[31:0]]; // word aligned
-
-	initial begin
-	    $readmemh("DataMemInit.mif", RAM);
-	end
+	assign rd2 = RAM[a1[31:0]]; // word aligned
+	 
+	 initial begin
+		$readmemh("DataMemInit.mif", RAM);
+	 end
 
     always_ff @(posedge clk)
         if (we) RAM[a[31:0]] <= wd;
